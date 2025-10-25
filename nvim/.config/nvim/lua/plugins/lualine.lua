ba4b519881b49df2083f0ace058cfb0089506e67
George Soulantikas <soulgeo01@gmail.com>
2025-10-25 13:08:09 +0300

    Custom neovim setup: changed lualine other bg colors

--- nvim/.config/nvim/lua/plugins/lualine.lua
+++ nvim/.config/nvim/lua/plugins/lualine.lua
@@ -2,7 +2,22 @@ return {
     "nvim-lualine/lualine.nvim",
     dependencies = { "nvim-tree/nvim-web-devicons" },
     config = function()
-        local custom = require("lualine.themes.auto")
+        local custom = require("lualine.themes.horizon")
+
+        custom.normal.a.bg = "#B4B4B4"
+        custom.insert.a.bg = "#4A8B8B"
+        custom.visual.a.bg = "#DF6464"
+        custom.command.a.bg = "#D87C4A"
+        custom.replace.a.bg = "#E49A44"
+        custom.inactive.a.bg = "none"
+
+        custom.normal.b.bg = "#121212"
+        custom.insert.b.bg = "#121212"
+        custom.visual.b.bg = "#121212"
+        custom.command.b.bg = "#121212"
+        custom.replace.b.bg = "#121212"
+        custom.inactive.b.bg = "#121212"
+
         custom.normal.c.bg = "none"
         custom.insert.c.bg = "none"
         custom.visual.c.bg = "none"
