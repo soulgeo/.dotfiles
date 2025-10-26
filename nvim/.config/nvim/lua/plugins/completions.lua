ed28b42fbed297206540549e6bac1ab6fcf69170
George Soulantikas <soulgeo01@gmail.com>
2025-10-26 13:02:02 +0200

    Custom neovim setup: completions: added Tab & Shift-Tab functionality

--- nvim/.config/nvim/lua/plugins/completions.lua
+++ nvim/.config/nvim/lua/plugins/completions.lua
@@ -10,13 +10,22 @@ return {
         "hrsh7th/nvim-cmp",
         config = function()
             local cmp = require("cmp")
+            local luasnip = require("luasnip")
+
+            -- load vscode-style snippets
             require("luasnip.loaders.from_vscode").lazy_load()
+
+            local has_words_before = function()
+                local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+                return col ~= 0
+                    and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+            end
 
             cmp.setup({
                 snippet = {
+                    -- use only luasnip (don't call vsnip and luasnip both)
                     expand = function(args)
-                        vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
-                        require("luasnip").lsp_expand(args.body) -- For `luasnip` users.
+                        require("luasnip").lsp_expand(args.body)
                     end,
                 },
                 window = {
@@ -29,6 +38,29 @@ return {
                     ["<C-Space>"] = cmp.mapping.complete(),
                     ["<C-a>"] = cmp.mapping.abort(),
                     ["<CR>"] = cmp.mapping.confirm({ select = true }),
+
+                    -- Tab / Shift-Tab behavior:
+                    ["<Tab>"] = cmp.mapping(function(fallback)
+                        if cmp.visible() then
+                            cmp.select_next_item()
+                        elseif luasnip.expand_or_jumpable() then
+                            luasnip.expand_or_jump()
+                        elseif has_words_before() then
+                            cmp.complete()
+                        else
+                            fallback()
+                        end
+                    end, { "i", "s" }),
+
+                    ["<S-Tab>"] = cmp.mapping(function(fallback)
+                        if cmp.visible() then
+                            cmp.select_prev_item()
+                        elseif luasnip.jumpable(-1) then
+                            luasnip.jump(-1)
+                        else
+                            fallback()
+                        end
+                    end, { "i", "s" }),
                 }),
                 sources = cmp.config.sources({
                     -- { name = "nvim_lsp" },
